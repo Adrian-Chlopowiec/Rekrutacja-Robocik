04e15776e199ca2d290f805407d3c878de520bfc
Adrian Chlopowiec <klopowik@op.pl>
2020-11-03 17:24:35 +0100

    Added docstring and t boundary in server.py

--- Zadanie 1/server.py
+++ Zadanie 1/server.py
@@ -5,6 +5,8 @@ from boat import Boat
 def main(n=5, t=1):
     if n < 5:
         raise Exception("N must be greater than 5")
+    if t <= 0:
+        raise Exception("T must be grater than 0")
 
     boat = Boat()
     boat.start_server(n, t)
@@ -12,6 +14,10 @@ def main(n=5, t=1):
 
 
 if __name__ == "__main__":
+    """
+    :param sys.argv[1]: N - number of coordinates to be sent by boat
+    :param sys.argv[2]: T - delay between sending coordinates in seconds    
+    """
     if len(sys.argv) == 3:
         main(int(sys.argv[1]), int(sys.argv[2]))
     else:
